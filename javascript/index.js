dee0a40c90525f870fd3c8b7d2bc87c4adf1c546
shamshersingh8574 <130432224+shamshersingh8574@users.noreply.github.com>
2023-05-02 17:53:18 +0530

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.testimonial').slick(
+        {
+            arrows: false,
+            centerMode: false,
+            centerPadding: '33px',
+            slidesToShow: 1,
+            dots: true,
+            speed: 1000,
+        }
+    );
+
+})
